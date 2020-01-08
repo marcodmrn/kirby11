e5f991cf9a1fe68ca3ef5128ecddd92f568a1e81
Marco Cours <marcocours@macbook-pro-de-marco.home>
2020-01-08 16:55:36 +0100

    modif

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -225,7 +225,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54794" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65044" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
