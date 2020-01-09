ef9815ed85ac50ac9a7b56779d061f6a18bfc0cf
Marco Cours <marcocours@MacBook-Pro-de-Marco.local>
2020-01-09 11:29:46 +0100

    random cards

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -315,7 +315,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53163" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54340" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
